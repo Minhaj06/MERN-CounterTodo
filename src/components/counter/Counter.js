f317b3070339ef59f5952f014d57f3a159feeef6
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-01-21 11:03:51 +0600

    initial commit

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../../redux/state/counter/counterSlice";
+import { decrement, increment, setCustom } from "../../redux/state/counter/counterSlice";
 
 const Counter = () => {
+  const customNumber = useRef();
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
@@ -12,24 +14,44 @@ const Counter = () => {
       </div>
 
       <div className="card-body">
-        <h1 className="mb-3">{count}</h1>
+        <h1>{count}</h1>
 
-        <button
-          onClick={() => {
-            dispatch(decrement());
-          }}
-          className="btn btn-danger me-3"
-        >
-          Decrease (-)
-        </button>
-        <button
-          onClick={() => {
-            dispatch(increment());
-          }}
-          className="btn btn-info text-white"
-        >
-          Increase (+)
-        </button>
+        <div className="my-4">
+          <button
+            onClick={() => {
+              dispatch(decrement());
+            }}
+            className="btn btn-danger me-3"
+          >
+            Decrease (-)
+          </button>
+          <button
+            onClick={() => {
+              dispatch(increment());
+            }}
+            className="btn btn-info text-white"
+          >
+            Increase (+)
+          </button>
+        </div>
+
+        <div className="w-50">
+          <input
+            ref={customNumber}
+            type="number"
+            name=""
+            id=""
+            className="form-control mb-3"
+          />
+          <button
+            onClick={() => {
+              dispatch(setCustom(customNumber.current.value));
+            }}
+            className="btn btn-info text-white w-100"
+          >
+            Set Custom
+          </button>
+        </div>
       </div>
     </div>
   );
